ec6dce26d9e2ea17827cc3af84cae99a4c592a88
shivashankar1253 <varimallashankar@gmail.com>
2020-11-19 23:21:10 +0530

    first commit

---
+++ src/components/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+//cart
+const Cart=()=> {
+    return(
+        <div>
+            <h1>Items to be added later</h1>
+        </div>
+    )
+}
+export default Cart;
---
+++ src/components/Signin.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+//Signin component
+class Signin extends React.Component{
+    render(){
+    return(
+        <div className="signbox">
+            <h1>Signin</h1>
+            <input type="text" placeholder="username" onChange={this.props.handlechangeUsername}/><br/><br/>
+            <input type="password" placeholder="password" onChange={this.props.handlechangePassword}/><br/><br/>
+            <button onClick={this.props.handleEnter}>Enter</button>
+        </div>
+    )
+}
+}
+export default Signin;
